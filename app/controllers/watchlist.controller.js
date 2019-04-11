6317448eac11318fae6dbb30b4ff8e069852a63a
Purvil Bambharolia <purvil12c@Purvils-MacBook-Pro.local>
2019-04-10 21:46:18 -0400

    api docs added

---
+++ app/controllers/watchlist.controller.js
@@ -0,0 +1,34 @@
+const User = require('../models/user.model.js');
+
+exports.addToWatchlist = (req, res) => {
+  let userId = req.params.userId;
+  let movieId = req.params.movieId;
+
+  User.findById(userId)
+    .then(user=>{
+      user.watchlist.push({movieId: movieId});
+      return user.save();
+    })
+    .then(()=>res.status(200).send({message: "Added to watchlist successfully"}))
+    .catch(err=>res.status(500).send({message: err.message}));
+
+}
+
+exports.removeFromWatchlist = (req, res) => {
+  let userId = req.params.userId;
+  let movieId = req.params.movieId;
+
+  User.findById(userId)
+    .then(user=>{
+      let i = 0;
+      for(i = 0;i<user.watchlist.length;i++){
+        if(user.watchlist[i].movieId==movieId){
+          break;
+        }
+      }
+      user.watchlist.pull({_id: user.watchlist[i]._id, movieId: movieId});
+      return user.save();
+    })
+    .then(()=>res.status(200).send({message: "Remove from watchlist successfully"}))
+    .catch(err=>res.status(500).send({message: err.message}));
+}
